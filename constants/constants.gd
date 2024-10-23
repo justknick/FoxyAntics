81f933775170f7b90a51e404cf2af17735c62f8f
justknick <126913469+justknick@users.noreply.github.com>
2024-10-23 17:29:33 -0400

    Section 136.10-137.11 - Bullet Scene and Script // created bullet scene and script to apply logic

---
+++ constants/constants.gd
@@ -0,0 +1,12 @@
+class_name Constants
+
+# object maker will receive one of this to be instantiated
+enum ObjectType {
+	EXPLOSION,
+	PICKUP, 
+	BULLET_PLAYER, 
+	BULLET_ENEMY
+}
+
+const PLAYER_GROUP: String = "Player"
+const MOVEABLES_GROUP: String = "Moveables"
---
+++ scenes/bullet/bullet_base.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+class_name Bullet
+
+var _direction: Vector2 = Vector2.RIGHT
+var _life_span: float = 5.0 
+var _life_time: float = 0.0
+
+
+#func _ready() -> void: 
+	#setup(global_position, Vector2.RIGHT, 10.0, 10.0)
+
+
+func _process(delta: float) -> void:
+	position += _direction * delta 
+	check_bullet_life_span(delta)
+
+
+func check_bullet_life_span(delta: float) -> void: 
+	# increment the lifetime by the delta
+	_life_time += delta
+	# once the lifetime reaches lifespan, bullet will disappear
+	if _life_time > _life_span:
+		queue_free()
+		print("bullet gone")
+
+
+func setup(pos: Vector2, direction: Vector2, speed: float, lifespan: float) -> void: 
+	_direction = direction.normalized() * speed 
+	_life_span = lifespan
+	global_position = pos
+
+
+func _on_area_entered(area: Area2D) -> void:
+	queue_free()
+	print("bullet gone")
