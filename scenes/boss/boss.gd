7d4226054151bebd84a384b4f9a3ded9f751ecf2
justknick <126913469+justknick@users.noreply.github.com>
2024-11-12 21:57:40 -0500

    Section 165.39-166.40 - Boss Scene and Animation Trees, Boss Logic  //

---
+++ scenes/boss/boss.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+const TRIGGER_CONDITION: String = "parameters/conditions/on_trigger"
+
+@onready var animation_tree: AnimationTree = $AnimationTree
+
+
+func trigger() -> void: 
+	pass 
+
+
+func _on_trigger_area_entered(area: Area2D) -> void:
+	animation_tree[TRIGGER_CONDITION] = true
