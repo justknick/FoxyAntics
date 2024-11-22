f8e17fafe3da89c43921394aec1883a4fcfdeb59
justknick <126913469+justknick@users.noreply.github.com>
2024-11-22 18:55:44 -0500

    Section 167.41 - Boss Hit State //

--- scenes/boss/boss.gd
+++ scenes/boss/boss.gd
@@ -1,13 +1,38 @@
 extends Node2D
+# when hurt, travel to any states in state machine
+# consider giving boss invinsibility during "arrive" animation
 
 const TRIGGER_CONDITION: String = "parameters/conditions/on_trigger"
 
 @onready var animation_tree: AnimationTree = $AnimationTree
+@onready var state_machine: AnimationNodeStateMachinePlayback = $AnimationTree["parameters/playback"]
+
+var _is_invinsible: bool = false 
 
 
 func trigger() -> void: 
 	pass 
 
 
+func set_invinsible(flag: bool) -> void: 
+	# when enemy boss takes hit, go invinsible
+	# invinsible will become false at end of hurt animation in AnimationPlayer
+	_is_invinsible = flag
+	if flag == true: 
+		state_machine.travel("hurt")
+
+
+func take_damage() -> void: 
+	if _is_invinsible == true: 
+		return 
+	set_invinsible(true)
+	print("Boss: ouch")
+
+
 func _on_trigger_area_entered(area: Area2D) -> void:
 	animation_tree[TRIGGER_CONDITION] = true
+
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	# when area (player bullet) enters the hurtbox > take hit, go invinsible 
+	take_damage() 
